80c4d21a8dc7766cd3695f5bea2db6944eff1cb2
SadLunch <olaadeus12345z@gmail.com>
2024-10-23 16:36:24 +0100

    trying with localstorage

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 window.onload = () => {
-    let testEntitiesAdded = false;
+    let testEntitiesAdded = localStorage.getItem('entitiesAdded') || false;
     alert('If testing the lat/lon manual input on a mobile device, please turn off your GPS to avoid the real location being detected.');
     const el = document.querySelector("[gps-new-camera]");
     el.addEventListener("gps-camera-update-position", e => {
-        if(!testEntitiesAdded) {
+        if(!testEntitiesAdded) {6
             alert(`Got first GPS position: lon ${e.detail.position.longitude} lat ${e.detail.position.latitude}`);
             // Add four boxes to the north (red), south (yellow), west (blue)
             // and east (red) of the initial GPS position
@@ -44,7 +44,7 @@ window.onload = () => {
                 
                 document.querySelector("a-scene").appendChild(entity);
             }
-            testEntitiesAdded = true;
+            localStorage.setItem('entitiesAdded', true);
         }
     });
 };
