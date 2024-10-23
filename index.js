ee5b3f1580662194041dedc2bf0506fdf7c34e37
SadLunch <olaadeus12345z@gmail.com>
2024-10-23 16:49:49 +0100

    localstorage was not the way to go

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
 window.onload = () => {
-    let testEntitiesAdded = localStorage.getItem('entitiesAdded') || false;
+    let testEntitiesAdded = false;
     alert('If testing the lat/lon manual input on a mobile device, please turn off your GPS to avoid the real location being detected.');
     const el = document.querySelector("[gps-new-camera]");
     el.addEventListener("gps-camera-update-position", e => {
-        if(!testEntitiesAdded) {6
+        if(!testEntitiesAdded) {
             alert(`Got first GPS position: lon ${e.detail.position.longitude} lat ${e.detail.position.latitude}`);
             // Add four boxes to the north (red), south (yellow), west (blue)
             // and east (red) of the initial GPS position
             const properties = [{
                     color: 'red', // North
-                    latDis: 0.00001,
+                    latDis: 0.0001,
                     lonDis: 0
                 },{
                     color: 'yellow', // South
-                    latDis: -0.00001,
+                    latDis: -0.0001,
                     lonDis: 0
                 },{
                     color: 'blue', // West
                     latDis: 0,
-                    lonDis: -0.00001
+                    lonDis: -0.0001
                 },{
                     color: 'green', // East
                     latDis: 0,
-                    lonDis: 0.00001
+                    lonDis: 0.0001
                 }
             ];
             for(const prop of properties) {
@@ -44,7 +44,7 @@ window.onload = () => {
                 
                 document.querySelector("a-scene").appendChild(entity);
             }
-            localStorage.setItem('entitiesAdded', true);
+            testEntitiesAdded = true;
         }
     });
 };
